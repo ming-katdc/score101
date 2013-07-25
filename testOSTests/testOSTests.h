a8943a8607b7f879fef997e2cc170e5c1f9be02a
Ming-der Wang <mingder78@yahoo.com>
2013-07-25 12:51:37 +0800

    init

---
+++ testOSTests/testOSTests.h
@@ -0,0 +1,13 @@
+//
+//  testOSTests.h
+//  testOSTests
+//
+//  Created by Ming-der Wang on 7/25/13.
+//  Copyright (c) 2013 Ming-der Wang. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface testOSTests : SenTestCase
+
+@end
---
+++ testOSTests/testOSTests.m
@@ -0,0 +1,32 @@
+//
+//  testOSTests.m
+//  testOSTests
+//
+//  Created by Ming-der Wang on 7/25/13.
+//  Copyright (c) 2013 Ming-der Wang. All rights reserved.
+//
+
+#import "testOSTests.h"
+
+@implementation testOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in testOSTests");
+}
+
+@end
